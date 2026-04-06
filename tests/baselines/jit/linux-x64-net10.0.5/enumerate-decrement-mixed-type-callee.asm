e15433976fe4437450d5db9de5669ccf09123613
Cyrus Najmabadi <Cyrus Najmabadi>
2026-04-06 12:51:41 +0200

    Fix linux-x64 JIT callee baselines (preserve instruction indentation)

--- tests/baselines/jit/linux-x64-net10.0.5/enumerate-decrement-mixed-type-callee.asm
+++ tests/baselines/jit/linux-x64-net10.0.5/enumerate-decrement-mixed-type-callee.asm
@@ -7,46 +7,46 @@
 ; No PGO data
 
 G_M000_IG01:                ;; offset=0x0000
-push     rbp
-push     r15
-push     rbx
-lea      rbp, [rsp+0x10]
-mov      rbx, rsi
-mov      r15, rdx
-
+       push     rbp
+       push     r15
+       push     rbx
+       lea      rbp, [rsp+0x10]
+       mov      rbx, rsi
+       mov      r15, rdx
+ 
 G_M000_IG02:                ;; offset=0x000F
-cmp      byte  ptr [rbx], bl
-mov      rdi, rbx
-call     [Fabrica.Core.Memory.Handle`1[MixedNode]:get_IsValid():bool:this]
-test     eax, eax
-je       SHORT G_M000_IG04
-
+       cmp      byte  ptr [rbx], bl
+       mov      rdi, rbx
+       call     [Fabrica.Core.Memory.Handle`1[MixedNode]:get_IsValid():bool:this]
+       test     eax, eax
+       je       SHORT G_M000_IG04
+ 
 G_M000_IG03:                ;; offset=0x001E
-mov      esi, dword ptr [rbx]
-mov      rdi, r15
-call     [Fabrica.Core.Memory.RefCountTable`1+DecrementNodeRefCountVisitor`1[MixedNode,MixedHandler]:Visit[MixedNode](Fabrica.Core.Memory.Handle`1[MixedNode]):this]
-
+       mov      esi, dword ptr [rbx]
+       mov      rdi, r15
+       call     [Fabrica.Core.Memory.RefCountTable`1+DecrementNodeRefCountVisitor`1[MixedNode,MixedHandler]:Visit[MixedNode](Fabrica.Core.Memory.Handle`1[MixedNode]):this]
+ 
 G_M000_IG04:                ;; offset=0x0029
-lea      rdi, bword ptr [rbx+0x04]
-call     [Fabrica.Core.Memory.Handle`1[OtherNode]:get_IsValid():bool:this]
-test     eax, eax
-jne      SHORT G_M000_IG06
-
+       lea      rdi, bword ptr [rbx+0x04]
+       call     [Fabrica.Core.Memory.Handle`1[OtherNode]:get_IsValid():bool:this]
+       test     eax, eax
+       jne      SHORT G_M000_IG06
+ 
 G_M000_IG05:                ;; offset=0x0037
-pop      rbx
-pop      r15
-pop      rbp
-ret      
-
+       pop      rbx
+       pop      r15
+       pop      rbp
+       ret      
+ 
 G_M000_IG06:                ;; offset=0x003C
-mov      esi, dword ptr [rbx+0x04]
-mov      rdi, r15
-
+       mov      esi, dword ptr [rbx+0x04]
+       mov      rdi, r15
+ 
 G_M000_IG07:                ;; offset=0x0042
-pop      rbx
-pop      r15
-pop      rbp
-tail.jmp [Fabrica.Core.Memory.RefCountTable`1+DecrementNodeRefCountVisitor`1[MixedNode,MixedHandler]:Visit[OtherNode](Fabrica.Core.Memory.Handle`1[OtherNode]):this]
-
+       pop      rbx
+       pop      r15
+       pop      rbp
+       tail.jmp [Fabrica.Core.Memory.RefCountTable`1+DecrementNodeRefCountVisitor`1[MixedNode,MixedHandler]:Visit[OtherNode](Fabrica.Core.Memory.Handle`1[OtherNode]):this]
+ 
 ; Total bytes of code 76
 
