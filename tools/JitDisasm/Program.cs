44e70ebe0bf50b4175eb3f419c8e96b18c19255f
Cyrus Najmabadi <Cyrus Najmabadi>
2026-04-10 01:07:17 +0200

    Update JitDisasm tooling and capture 2026-04-10 disassembly
    - Update Program.cs harness to match current benchmark (4 uniform
      phases of 192 ComputeJobs)
    - Add dump-jit-disasm.py script that captures per-method JIT disasm
      for all scheduling hot-path methods
    - Add 2026-04-10/ snapshot of ARM64 disassembly from Apple M4 Max

--- tools/JitDisasm/Program.cs
+++ tools/JitDisasm/Program.cs
@@ -1,125 +1,48 @@
 using Fabrica.Core.Jobs;
-using Fabrica.Core.Memory;
 using Fabrica.SampleGame.Benchmarks.Scale;
 
-const int Phase1Count = 192;
-const int Phase2Count = 192;
-const int Phase3Count = 96;
-const int Phase4Count = 48;
-const int SnapshotNodeCount = 10;
+const int JobsPerPhase = 192;
+const int PhaseCount = 4;
 const int ComputeIterations = 25_000;
 
 var pool = new WorkerPool(coordinatorCount: 1);
 var scheduler = new JobScheduler(pool);
-var store = new GlobalNodeStore<BenchNode, BenchNodeOps>(pool.WorkerCount);
-store.SetNodeOps(new BenchNodeOps { Store = store });
-var coordinator = new MergeCoordinator([store]);
 
 var trigger = new TriggerJob();
+var phases = new ComputeJob[PhaseCount][];
+var barriers = new BarrierJob[PhaseCount];
 
-var phase1 = new ComputeJob[Phase1Count];
-for (var i = 0; i < Phase1Count; i++)
-    phase1[i] = new ComputeJob();
-
-var barrier1 = new BarrierJob();
-
-var phase2 = new ComputeJob[Phase2Count];
-for (var i = 0; i < Phase2Count; i++)
-    phase2[i] = new ComputeJob();
-
-var barrier2 = new BarrierJob();
-
-var phase3 = new ComputeJob[Phase3Count];
-for (var i = 0; i < Phase3Count; i++)
-    phase3[i] = new ComputeJob();
-
-var barrier3 = new BarrierJob();
-
-var phase4 = new SnapshotJob[Phase4Count];
-for (var i = 0; i < Phase4Count; i++)
-    phase4[i] = new SnapshotJob();
-
-var collector = new SnapshotCollectorJob { Sources = phase4 };
-
-var buffers = store.ThreadLocalBuffers;
-var hasPreviousSlice = false;
-SnapshotSlice<BenchNode, BenchNodeOps> previousSlice = default;
+for (var p = 0; p < PhaseCount; p++)
+{
+    phases[p] = new ComputeJob[JobsPerPhase];
+    for (var i = 0; i < JobsPerPhase; i++)
+        phases[p][i] = new ComputeJob();
+    barriers[p] = new BarrierJob();
+}
 
 for (var tick = 0; tick < 3; tick++)
 {
     trigger.Reset();
 
-    for (var i = 0; i < phase1.Length; i++)
+    for (var p = 0; p < PhaseCount; p++)
     {
-        var job = phase1[i];
-        job.Reset();
-        job.Iterations = ComputeIterations;
-        job.Seed = i;
-        job.DependsOn(trigger);
+        var prev = p == 0 ? (Job)trigger : barriers[p - 1];
+        for (var i = 0; i < JobsPerPhase; i++)
+        {
+            var job = phases[p][i];
+            job.Reset();
+            job.Iterations = ComputeIterations;
+            job.Seed = p * JobsPerPhase + i;
+            job.DependsOn(prev);
+        }
+
+        barriers[p].Reset();
+        for (var i = 0; i < JobsPerPhase; i++)
+            barriers[p].DependsOn(phases[p][i]);
     }
-
-    barrier1.Reset();
-    for (var i = 0; i < phase1.Length; i++)
-        barrier1.DependsOn(phase1[i]);
-
-    for (var i = 0; i < phase2.Length; i++)
-    {
-        var job = phase2[i];
-        job.Reset();
-        job.Iterations = ComputeIterations;
-        job.Seed = Phase1Count + i;
-        job.DependsOn(barrier1);
-    }
-
-    barrier2.Reset();
-    for (var i = 0; i < phase2.Length; i++)
-        barrier2.DependsOn(phase2[i]);
-
-    for (var i = 0; i < phase3.Length; i++)
-    {
-        var job = phase3[i];
-        job.Reset();
-        job.Iterations = ComputeIterations;
-        job.Seed = Phase1Count + Phase2Count + i;
-        job.DependsOn(barrier2);
-    }
-
-    barrier3.Reset();
-    for (var i = 0; i < phase3.Length; i++)
-        barrier3.DependsOn(phase3[i]);
-
-    for (var i = 0; i < phase4.Length; i++)
-    {
-        var job = phase4[i];
-        job.Reset();
-        job.Iterations = ComputeIterations;
-        job.Seed = Phase1Count + Phase2Count + Phase3Count + i;
-        job.Buffers = buffers;
-        job.NodeCount = SnapshotNodeCount;
-        job.IsRoot = false;
-        job.DependsOn(barrier3);
-    }
-
-    collector.Reset();
-    collector.Buffers = buffers;
-    for (var i = 0; i < phase4.Length; i++)
-        collector.DependsOn(phase4[i]);
 
     scheduler.Submit(trigger);
-
-    SnapshotSlice<BenchNode, BenchNodeOps> slice;
-    using (var merge = coordinator.MergeAll())
-        slice = store.BuildSnapshotSlice();
-
-    if (hasPreviousSlice)
-        store.ReleaseSnapshotSlice(previousSlice);
-
-    previousSlice = slice;
-    hasPreviousSlice = true;
 }
-
-if (hasPreviousSlice)
-    store.ReleaseSnapshotSlice(previousSlice);
 
 pool.Dispose();
 Console.WriteLine("JIT disasm harness complete.");
